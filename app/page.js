749a6147c9efd5e72ec4522d7cc0d6a0e75072a0
Rohit <freelancerrohit46@gmail.com>
2025-08-13 00:08:20 +0530

    something more formated

--- app/page.js
+++ app/page.js
@@ -173,7 +173,7 @@ export default function YouTubeHome() {
   // formating duration
   function formatDuration(seconds) {
     const m = Math.floor(seconds / 60);
-    const s = seconds % 60;
+    const s = Math.floor(seconds % 60);
     return `${m}:${s.toString().padStart(2, "0")}`;
   }
 
